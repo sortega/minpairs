da69e9fbd05a04a478032fe62b571ba29222f1a8
Sebastián Ortega <sebastian.ortega@letgo.com>
2020-07-01 06:53:34 +0200

    Build

---
+++ build/precache-manifest.1c45c6d311686b8709a5f17ca0720ccb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f843a8f576a992ed74b5e5d00417c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea1c8362c1c0bb2bf97",
+    "url": "/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "ca4b75e815f30c239ea6",
+    "url": "/static/css/main.8262b767.chunk.css"
+  },
+  {
+    "revision": "fea1c8362c1c0bb2bf97",
+    "url": "/static/js/2.68d03062.chunk.js"
+  },
+  {
+    "revision": "6ebec66d43fc3be4876428325bc92b89",
+    "url": "/static/js/2.68d03062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4b75e815f30c239ea6",
+    "url": "/static/js/main.365aa945.chunk.js"
+  },
+  {
+    "revision": "d783fa108e945674fb18",
+    "url": "/static/js/runtime-main.e3c2ed68.js"
+  }
+]);
