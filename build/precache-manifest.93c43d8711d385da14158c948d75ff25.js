1fcf155129ac4278c6c1fb708fc78dea17a6014b
Sebastián Ortega <sebastian.ortega@letgo.com>
2020-07-30 19:53:31 +0200

    Build

--- build/precache-manifest.93c43d8711d385da14158c948d75ff25.js
+++ build/precache-manifest.93c43d8711d385da14158c948d75ff25.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ca2e4551b9d4c2527792992a047490f",
+    "revision": "cec98a4e1ff7572cfdaab6ea74bf3602",
     "url": "/index.html"
   },
   {
-    "revision": "fea1c8362c1c0bb2bf97",
+    "revision": "5fc1f0ba7f2c294fc0fd",
     "url": "/static/css/2.8d06fd47.chunk.css"
   },
   {
-    "revision": "d260067396dfe9182439",
+    "revision": "74987c92d9af11e26779",
     "url": "/static/css/main.8262b767.chunk.css"
   },
   {
-    "revision": "fea1c8362c1c0bb2bf97",
-    "url": "/static/js/2.68d03062.chunk.js"
+    "revision": "5fc1f0ba7f2c294fc0fd",
+    "url": "/static/js/2.d2fdcabc.chunk.js"
   },
   {
     "revision": "6ebec66d43fc3be4876428325bc92b89",
-    "url": "/static/js/2.68d03062.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d2fdcabc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d260067396dfe9182439",
-    "url": "/static/js/main.bca8375b.chunk.js"
+    "revision": "74987c92d9af11e26779",
+    "url": "/static/js/main.562e71f4.chunk.js"
   },
   {
     "revision": "d783fa108e945674fb18",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e8b20c59a41be2740aa5d32f4a1e8ec.js"
+  "/precache-manifest.93c43d8711d385da14158c948d75ff25.js"
 );
 
 self.addEventListener('message', (event) => {
